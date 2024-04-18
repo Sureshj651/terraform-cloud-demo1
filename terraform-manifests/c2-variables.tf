b8d5cee40361ca7385b64d2b583cfe9fc203ae26
Sureshj651 <165656355+Sureshj651@users.noreply.github.com>
2024-04-18 11:40:39 +0530

    Update c2-variables.tf

--- terraform-manifests/c2-variables.tf
+++ terraform-manifests/c2-variables.tf
@@ -23,33 +23,6 @@ variable "resoure_group_location" {
   type = string
   #default = "eastus"
 }
-# 5. Virtual Network Name
-variable "virtual_network_name" {
-  description = "Virtual Network Name"
-  type = string 
-  default = "myvnet"
-}
-# 6. Subnet Name
-variable "subnet_name" {
-  description = "Virtual Network Subnet Name"
-  type = string 
-}
-# 7. Public IP Name
-variable "publicip_name" {
-  description = "Public IP Name"
-  type = string 
-}
-# 8. Network Interfance
-variable "network_interface_name" {
-  description = "Network Interface Name"
-  type = string 
-}
-
-# 9. Virtual Machine Name
-variable "virtual_machine_name" {
-  description = "Virtual Machine Name"
-  type = string 
-}
 
 
 
